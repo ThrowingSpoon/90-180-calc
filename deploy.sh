0d10ee63a28a235851c1bd7742c665dc8608e7fe
Liam <liam@liammartin.co.uk>
2025-01-23 00:43:53 +0000

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,8 @@ git pull --all
 git checkout master
 git reset --hard HEAD
 git pull --all
+touch docker/production/acme.json
+chmod 600 docker/production/acme.json
 make stop-production
+make build-production
 make start-production
